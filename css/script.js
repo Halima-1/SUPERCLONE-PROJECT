5b4589ac3bdea51dc1bd37d5bd26235e4ff62746
Halima-1 <105408210+Halima-1@users.noreply.github.com>
2024-01-30 03:45:11 +0100

    Add files via upload

--- css/script.js
+++ css/script.js
@@ -1,7 +1,7 @@
 function display(){
     
     // home
-    let display= document.getElementById('hom');
+    let display= document.getElementById('sideNav');
     if(display.classList.contains('show')){
         display.classList.remove('show');
         display.classList.add('hide');
@@ -11,76 +11,76 @@ function display(){
     }
     
     // airtime
-    let airtime = document.getElementById('air');
-    if(airtime.classList.contains('show')){
-        airtime.classList.remove('show');
-        airtime.classList.add('hide');
-    }else{
-        airtime.classList.remove('hide');
-        airtime.classList.add('show');
-    }
+    // let airtime = document.getElementById('air');
+    // if(airtime.classList.contains('show')){
+    //     airtime.classList.remove('show');
+    //     airtime.classList.add('hide');
+    // }else{
+    //     airtime.classList.remove('hide');
+    //     airtime.classList.add('show');
+    // }
     
-    // buy data
-    let data = document.getElementById('dat');
-    if(data.classList.contains('show')){
-        data.classList.remove('show');
-        data.classList.add('hide');
-    }else{
-        data.classList.remove('hide');
-        data.classList.add('show');
-    }
+    // // buy data
+    // let data = document.getElementById('dat');
+    // if(data.classList.contains('show')){
+    //     data.classList.remove('show');
+    //     data.classList.add('hide');
+    // }else{
+    //     data.classList.remove('hide');
+    //     data.classList.add('show');
+    // }
     
-    // login
-    let login = document.getElementById('log');
-    if(login.classList.contains('show')){
-        login.classList.remove('show');
-        login.classList.add('hide');
-    }else{
-        login.classList.remove('hide');
-        login.classList.add('show');
-    }
+    // // login
+    // let login = document.getElementById('log');
+    // if(login.classList.contains('show')){
+    //     login.classList.remove('show');
+    //     login.classList.add('hide');
+    // }else{
+    //     login.classList.remove('hide');
+    //     login.classList.add('show');
+    // }
     
-    // log out
-    let logout = document.getElementById('outt')
-    if(logout.classList.contains('show')){
-        logout.classList.remove('show');
-        logout.classList.add('hide');
-    }else{
-        logout.classList.remove('hide');
-        logout.classList.add('show');
-    }
+    // // log out
+    // let logout = document.getElementById('outt')
+    // if(logout.classList.contains('show')){
+    //     logout.classList.remove('show');
+    //     logout.classList.add('hide');
+    // }else{
+    //     logout.classList.remove('hide');
+    //     logout.classList.add('show');
+    // }
     
-    // register
-    let reg = document.getElementById('regg')
-    if(reg.classList.contains('show')){
-        reg.classList.remove('show');
-        reg.classList.add('hide');
-    }else{
-        reg.classList.remove('hide');
-        reg.classList.add('show');
-    }
+    // // register
+    // let reg = document.getElementById('regg')
+    // if(reg.classList.contains('show')){
+    //     reg.classList.remove('show');
+    //     reg.classList.add('hide');
+    // }else{
+    //     reg.classList.remove('hide');
+    //     reg.classList.add('show');
+    // }
     
-    // setting
-    let setting = document.getElementById('setting')
-    if(setting.classList.contains('show')){
-        setting.classList.remove('show');
-        setting.classList.add('hide');
-    }else{
-        setting.classList.remove('hide');
-        setting.classList.add('show');
-    }
+    // // setting
+    // let setting = document.getElementById('setting')
+    // if(setting.classList.contains('show')){
+    //     setting.classList.remove('show');
+    //     setting.classList.add('hide');
+    // }else{
+    //     setting.classList.remove('hide');
+    //     setting.classList.add('show');
+    // }
     
-    // display.classList.toggle('show')
-    let sideNav = document.getElementById('sideNav')
-    if (sideNav.classList.contains('showing')){
-        sideNav.classList.remove('showing');
-        sideNav.classList.add('hidden');
-    }
-    else{
-        sideNav.classList.add('showing');
-        sideNav.classList.remove('hidden');
+    // // display.classList.toggle('show')
+    // let sideNav = document.getElementById('sideNav')
+    // if (sideNav.classList.contains('showing')){
+    //     sideNav.classList.remove('showing');
+    //     sideNav.classList.add('hidden');
+    // }
+    // else{
+    //     sideNav.classList.add('showing');
+    //     sideNav.classList.remove('hidden');
 
-    }
+    // }
        }
 
        fetch("https://randomuser.me/api/").then(function(data){
